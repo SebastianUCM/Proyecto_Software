cba9903b4acd7f8d1635a22315d35236d415aa72
SebastianUCM <s.cerpa.gutierrez@gmail.com>
2020-06-30 23:24:15 -0400

    Reserva_Laboratorio_1.3 - Se Agregan Seeder para "Carrera"

--- database/seeds/CarreraSeeder.php
+++ database/seeds/CarreraSeeder.php
@@ -14,11 +14,51 @@ class CarreraSeeder extends Seeder
     {
         DB::table('carreras')->insert([
             'codigo' => 'ICI',
+            'nombre' => 'Ingenieria Civil',
+        ]);
+        DB::table('carreras')->insert([
+            'codigo' => 'ICO',
+            'nombre' => 'Ingenieria en Construcción',
+        ]);
+        DB::table('carreras')->insert([
+            'codigo' => 'INF',
             'nombre' => 'Ingenieria Civil Informatica',
+        ]);
+        DB::table('carreras')->insert([
+            'codigo' => 'PGB - T',
+            'nombre' => 'Pedagogía en Educación General Básica con Mención - Talca',
+        ]);
+        DB::table('carreras')->insert([
+            'codigo' => 'PGB - C',
+            'nombre' => 'Pedagogía en Educación General Básica con Mención - Curicó',
+        ]);        
+        DB::table('carreras')->insert([
+            'codigo' => 'PIN',
+            'nombre' => 'Pedagogía en Inglés',
+        ]);       
+        DB::table('carreras')->insert([
+            'codigo' => 'MED',
+            'nombre' => 'Medicina',
+        ]);       
+        DB::table('carreras')->insert([
+            'codigo' => 'ENF - T',
+            'nombre' => 'Enfermeria - Talca',
+        ]);       
+        DB::table('carreras')->insert([
+            'codigo' => 'ENF - C',
+            'nombre' => 'Enfermeria - Curicó',
+        ]);     
+        DB::table('carreras')->insert([
+            'codigo' => 'PMC',
+            'nombre' => 'Pedagogía en Matemáticas y Computación',
         ]);
         DB::table('carreras')->insert([
             'codigo' => 'EFI',
             'nombre' => 'Educacion Fisica',
+        ]);
+        DB::table('carreras')->insert([
+            'codigo' => 'LCC',
+            'nombre' => 'Pedagogía en Lengua Castellana y Comunicación',
         ]);
     }
 }
