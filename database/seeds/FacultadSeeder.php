8ac4fec35884a757169915f07b91553da6ee7d8a
SebastianUCM <s.cerpa.gutierrez@gmail.com>
2020-06-30 23:47:32 -0400

    Se Agrega la Tabla "Facultad"

---
+++ database/seeds/FacultadSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FacultadSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
