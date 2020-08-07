74c7b227e03445d7f0a9197e8f38ac5270d37743
SebastianM1903 <seba.r.f.v@gmail.com>
2020-08-06 22:03:52 -0400

    Prueba Seeder Sebastian_F

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,12 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('sc261897'),
             'rol' => 'administrador',
         ]);
+        DB::table('users')->insert([
+            'name' => 'Sebastian Fuenzalida',
+            'email' => 's@gmail.com',
+            'password' => bcrypt('123'),
+            'rol' => 'administrador',
+        ]);
 
         $this->call(ColorSeeder::class);
         $this->call(CarreraSeeder::class);
